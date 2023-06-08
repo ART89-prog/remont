a9ef877e956ac2d4ae928d8357c8122f2c893bea
ART89-prog <dykpgilkom@mail.ru>
2023-06-08 18:53:21 +0300

    4756567546754

--- js/scripts.js
+++ js/scripts.js
@@ -203,47 +203,44 @@ $(() => {
 
 
 
-	// Инициализация превью слайдера
-	const sliderThumbs = new Swiper('.slider__thumbs .swiper-container', { // ищем слайдер превью по селектору
-		// задаем параметры
-		direction: 'vertical', // вертикальная прокрутка
-		slidesPerView: 3, // показывать по 3 превью
-		spaceBetween: 24, // расстояние между слайдами
-		navigation: { // задаем кнопки навигации
-			nextEl: '.slider__next', // кнопка Next
-			prevEl: '.slider__prev' // кнопка Prev
+	
+	const sliderThumbs = new Swiper('.slider__thumbs .swiper-container', { 
+		direction: 'vertical', 
+		slidesPerView: 3, 
+		spaceBetween: 8, 
+		navigation: { 
+			nextEl: '.slider__next', 
+			prevEl: '.slider__prev'
 		},
-		freeMode: true, // при перетаскивании превью ведет себя как при скролле
-		breakpoints: { // условия для разных размеров окна браузера
-			0: { // при 0px и выше
-				direction: 'horizontal', // горизонтальная прокрутка
+		freeMode: true, 
+		breakpoints: { 
+			0: { 
+				direction: 'horizontal', 
 			},
-			768: { // при 768px и выше
-				direction: 'vertical', // вертикальная прокрутка
+			768: { 
+				direction: 'vertical', 
 			}
 		}
 	});
-	// Инициализация слайдера изображений
-	const sliderImages = new Swiper('.slider__images .swiper-container', { // ищем слайдер превью по селектору
-		// задаем параметры
-		direction: 'vertical', // вертикальная прокрутка
-		slidesPerView: 1, // показывать по 1 изображению
-		spaceBetween: 32, // расстояние между слайдами
-		mousewheel: true, // можно прокручивать изображения колёсиком мыши
-		navigation: { // задаем кнопки навигации
-			nextEl: '.slider__next', // кнопка Next
-			prevEl: '.slider__prev' // кнопка Prev
+	const sliderImages = new Swiper('.slider__images .swiper-container', {
+		direction: 'vertical', 
+		slidesPerView: 1,
+		spaceBetween: 8,
+		mousewheel: true, 
+		navigation: { 
+			nextEl: '.slider__next',
+			prevEl: '.slider__prev' 
 		},
-		grabCursor: true, // менять иконку курсора
-		thumbs: { // указываем на превью слайдер
-			swiper: sliderThumbs // указываем имя превью слайдера
+		grabCursor: true, 
+		thumbs: {
+			swiper: sliderThumbs
 		},
-		breakpoints: { // условия для разных размеров окна браузера
-			0: { // при 0px и выше
-				direction: 'horizontal', // горизонтальная прокрутка
+		breakpoints: { 
+			0: {
+				direction: 'horizontal', 
 			},
-			768: { // при 768px и выше
-				direction: 'vertical', // вертикальная прокрутка
+			768: { 
+				direction: 'vertical', 
 			}
 		}
 	});
